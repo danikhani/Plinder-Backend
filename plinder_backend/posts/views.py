e76af54c8cf49c18f88ea1eedd74072a90344199
DANIAL HEZARKHANI <danial.hezarkhani@rwth-aachen.de>
2020-06-04 18:44:22 +0200

    added put,delet,post,get in views.py.

--- plinder_backend/posts/views.py
+++ plinder_backend/posts/views.py
@@ -10,19 +10,26 @@ from .serializers import PostSerializer
 @api_view(['GET', 'DELETE', 'PUT'])
 def get_delete_update_post(request, pk):
     try:
-        puppy = Post.objects.get(pk=pk)
+        post = Post.objects.get(pk=pk)
     except Post.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     # get details of a single Post
     if request.method == 'GET':
-        return Response({})
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
     # delete a single Post
     elif request.method == 'DELETE':
-        return Response({})
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     # update details of a single Post
+    # similar to an insert, we serialize and validate the request data and then respond appropriately.
     elif request.method == 'PUT':
-        return Response({})
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
@@ -33,5 +40,17 @@ def get_post_post(request):
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
     # insert a new record for a post
+    # Here, we inserted a new record by serializing and validating the request data before inserting to the database.
     elif request.method == 'POST':
-        return Response({})
+        data = {
+            'name': request.data.get('name'),
+            'title': request.data.get('title'),
+            'content': request.data.get('content'),
+            'time_created': request.data.get('time_created'),
+            'time_updated' : request.data.get('time_updated'),
+        }
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
