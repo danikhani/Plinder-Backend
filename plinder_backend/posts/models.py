bda6827a03f3efd50fc6997a6f5afb2051a1fce9
DANIAL HEZARKHANI <danial.hezarkhani@rwth-aachen.de>
2020-06-07 12:49:48 +0200

    working status before importing account future

--- plinder_backend/posts/models.py
+++ plinder_backend/posts/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 
 class Post(models.Model):
+    class Meta:
+       # db_table = 'posts_table'
+        db_table = 'public\".\"posts_table'
+
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
