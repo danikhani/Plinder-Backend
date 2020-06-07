ce4e7bc476fde501f691bb6d09bbbfbd0d258a3e
Danial Hezarkhani <danial.hezarkhani@rwth-aachen.de>
2020-06-07 16:49:32 +0200

    Futures/post optimization

--- plinder_backend/posts/serializers.py
+++ plinder_backend/posts/serializers.py
@@ -1,12 +1,133 @@
 # posts/serializers.py
 from rest_framework import serializers
-from . import models
+from .models import Post
+
+import os
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+IMAGE_SIZE_MAX_BYTES = 1024 * 1024 * 2 # 2MB
+MIN_TITLE_LENGTH = 5
+MIN_BODY_LENGTH = 50
+
+from .utils import is_image_aspect_ratio_valid, is_image_size_valid
 
 
-# it is a model serializer
 class PostSerializer(serializers.ModelSerializer):
 
+    username = serializers.SerializerMethodField('get_username_from_author')
+    image 	 = serializers.SerializerMethodField('validate_image_url')
+
     class Meta:
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
-        model = models.Post
+        model = Post
+        fields = ['pk', 'title', 'slug', 'body', 'image', 'date_updated', 'username']
+
+    def get_username_from_author(self, post_post):
+        username = post_post.author.username
+        return username
+
+    def validate_image_url(self, post_post):
+        image = post_post.image
+        new_url = image.url
+        if "?" in new_url:
+            new_url = image.url[:image.url.rfind("?")]
+        return new_url
+
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'image']
+
+    def validate(self, post_post):
+        try:
+            title = post_post['title']
+            if len(title) < MIN_TITLE_LENGTH:
+                raise serializers.ValidationError(
+                    {"response": "Enter a title longer than " + str(MIN_TITLE_LENGTH) + " characters."})
+
+            body = post_post['body']
+            if len(body) < MIN_BODY_LENGTH:
+                raise serializers.ValidationError(
+                    {"response": "Enter a body longer than " + str(MIN_BODY_LENGTH) + " characters."})
+
+            image = post_post['image']
+            url = os.path.join(settings.TEMP, str(image))
+            storage = FileSystemStorage(location=url)
+
+            with storage.open('', 'wb+') as destination:
+                for chunk in image.chunks():
+                    destination.write(chunk)
+                destination.close()
+
+            # Check image size
+            if not is_image_size_valid(url, IMAGE_SIZE_MAX_BYTES):
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "That image is too large. Images must be less than 2 MB. Try a different image."})
+
+            # Check image aspect ratio
+            if not is_image_aspect_ratio_valid(url):
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "Image height must not exceed image width. Try a different image."})
+
+            os.remove(url)
+        except KeyError:
+            pass
+        return post_post
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'image', 'date_updated', 'author']
+
+    def save(self):
+
+        try:
+            image = self.validated_data['image']
+            title = self.validated_data['title']
+            if len(title) < MIN_TITLE_LENGTH:
+                raise serializers.ValidationError(
+                    {"response": "Enter a title longer than " + str(MIN_TITLE_LENGTH) + " characters."})
+
+            body = self.validated_data['body']
+            if len(body) < MIN_BODY_LENGTH:
+                raise serializers.ValidationError(
+                    {"response": "Enter a body longer than " + str(MIN_BODY_LENGTH) + " characters."})
+
+            post_post = Post(
+                author=self.validated_data['author'],
+                title=title,
+                body=body,
+                image=image,
+            )
+
+            url = os.path.join(settings.TEMP, str(image))
+            storage = FileSystemStorage(location=url)
+
+            with storage.open('', 'wb+') as destination:
+                for chunk in image.chunks():
+                    destination.write(chunk)
+                destination.close()
+
+            # Check image size
+            if not is_image_size_valid(url, IMAGE_SIZE_MAX_BYTES):
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "That image is too large. Images must be less than 2 MB. Try a different image."})
+
+            # Check image aspect ratio
+            if not is_image_aspect_ratio_valid(url):
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "Image height must not exceed image width. Try a different image."})
+
+            os.remove(url)
+            post_post.save()
+            return post_post
+        except KeyError:
+            raise serializers.ValidationError({"response": "You must have a title, some content, and an image."})
 
--- plinder_backend/posts/urls.py
+++ plinder_backend/posts/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from .views import(
+    api_detail_post_view,
+    api_update_post_view,
+    api_delete_post_view,
+    api_create_post_view,
+    api_is_author_of_post,
+    ApiPostListView
+)
 
-from . import views
+app_name = 'post'
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),
+    path('<slug>/', api_detail_post_view, name="detail"),
+    path('<slug>/update', api_update_post_view, name="update"),
+    path('<slug>/delete', api_delete_post_view, name="delete"),
+    path('create', api_create_post_view, name="create"),
+    path('list', ApiPostListView.as_view(), name="list"),
+    path('<slug>/is_author', api_is_author_of_post, name="is_author"),
 ]
