ce864879271b5412778c106a70a7999f8164f867
DANIAL HEZARKHANI <danial.hezarkhani@rwth-aachen.de>
2020-06-07 14:41:52 +0200

    added account api.

--- plinder_backend/pl_api/urls.py
+++ plinder_backend/pl_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('posts.urls')),
+    path('api/post/', include('posts.urls')),
+    path('api/account/', include('account.urls', 'account_api')),
 ]
--- plinder_backend/posts/urls.py
+++ plinder_backend/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/', views.PostList.as_view()),
-    path('post/<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
